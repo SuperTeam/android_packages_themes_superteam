54e9be19126b79c2e3aca63839cf1cddd9a76fa9
podxboq <podxboq@gmail.com>
2013-03-15 19:33:50 +0100

    First commit for SuperOSR build

--- Android.mk
+++ Android.mk
@@ -3,10 +3,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PACKAGE_NAME := N7Theme
+LOCAL_PACKAGE_NAME := SuperTeamTheme
 
-#LOCAL_MODULE := io.n7.themes
-
-LOCAL_AAPT_FLAGS := -c hdpi -x 10
+LOCAL_AAPT_FLAGS := -c mdpi,hdpi -x 10
 
 include $(BUILD_PACKAGE)
